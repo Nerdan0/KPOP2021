e755214cef28561892d5ad54f802f1ff67fd8708
Nerdan <01castle02@gmail.com>
2021-09-15 02:56:20 +0300

    hospodi pomogi

--- Lab1/Task1-1.js
+++ Lab1/Task1-1.js
@@ -1,9 +1,17 @@
+var Category;
+(function (Category) {
+    Category["Analyst"] = "Business analyst";
+    Category["Developer"] = "Developer";
+    Category["Designer"] = "Designer";
+    Category["QA"] = "QA";
+    Category["Scrum"] = "Scrum master";
+})(Category || (Category = {}));
 function getAllworkers() {
     var workers = [
-        { Name: 'Ivan', surname: 'Ivanov', available: true, salary: 1000 },
-        { Name: 'Petro', surname: 'Petrov', available: true, salary: 1500 },
-        { Name: 'Vasyl', surname: 'Vasyliev', available: false, salary: 1600 },
-        { Name: 'Evgen', surname: 'Zhukov', available: true, salary: 1300 }
+        { id: 1, Name: 'Ivan', surname: 'Ivanov', available: true, salary: 1000, category: Category.Analyst },
+        { id: 1, Name: 'Petro', surname: 'Petrov', available: true, salary: 1500, category: Category.Analyst },
+        { id: 1, Name: 'Vasyl', surname: 'Vasyliev', available: false, salary: 1600, category: Category.Scrum },
+        { id: 1, Name: 'Evgen', surname: 'Zhukov', available: true, salary: 1300, category: Category.Developer }
     ];
     return workers;
 }
@@ -12,7 +20,37 @@ function logFirstAvailable(workers) {
     console.log(workers.length);
     for (var _i = 0, workers_1 = workers; _i < workers_1.length; _i++) {
         var worker = workers_1[_i];
-        console.log(worker.Name + " " + worker.surname);
+        if (worker.available) {
+            console.log(worker.Name + " " + worker.surname);
+        }
     }
 }
+function getWorkersNamesByCategory(cat) {
+    var workers = [];
+    for (var _i = 0, _a = getAllworkers(); _i < _a.length; _i++) {
+        var worker = _a[_i];
+        if (worker.category == cat) {
+            workers.push(worker.Name + " " + worker.surname);
+        }
+    }
+    return workers;
+}
+function logWorkersNames(workers) {
+    console.log(workers);
+}
+/*let arrowPrintDeveloper = (workers:any[])=> workers.forEach(if(worker.category == cat)
+{
+    workers.push(worker.Name+` `+worker.surname);
+}    )*/
+/*let arrowPrintDeveloper = getAllworkers().forEach(worker => {if(worker.category == Category.Developer)
+{
+    console.log(worker.Name+` `+worker.surname);
+} })*/
 logFirstAvailable(getAllworkers());
+logWorkersNames(getWorkersNamesByCategory(Category.Analyst));
+//arrowPrintDeveloper
+getAllworkers().forEach(function (worker) {
+    if (worker.category == Category.Developer) {
+        console.log(worker.Name + " " + worker.surname);
+    }
+});
--- Lab1/Task1-1.ts
+++ Lab1/Task1-1.ts
@@ -1,15 +1,18 @@
+enum Category {Analyst = "Business analyst", Developer ="Developer",Designer = "Designer",QA = "QA",Scrum = "Scrum master"}
+
+
 function getAllworkers() 
 {
     let workers = [
-        {Name: 'Ivan', surname: 'Ivanov', available: true, salary: 1000},
-        {Name: 'Petro', surname: 'Petrov', available: true, salary: 1500},
-        {Name: 'Vasyl', surname: 'Vasyliev', available: false, salary: 1600},
-        {Name: 'Evgen', surname: 'Zhukov', available: true, salary: 1300}
+        {id: 1, Name: 'Ivan', surname: 'Ivanov', available: true, salary: 1000, category: Category.Analyst},
+        {id: 1, Name: 'Petro', surname: 'Petrov', available: true, salary: 1500, category: Category.Analyst},
+        {id: 1, Name: 'Vasyl', surname: 'Vasyliev', available: false, salary: 1600, category: Category.Scrum},
+        {id: 1, Name: 'Evgen', surname: 'Zhukov', available: true, salary: 1300, category: Category.Developer}
     ]
     return workers;
 }
    
-function logFirstAvailable(workers:any[])
+function logFirstAvailable(workers:any[]):void
 {
     //let workersNum:Number = workers.length
     console.log(workers.length)
@@ -18,12 +21,59 @@ function logFirstAvailable(workers:any[])
 
         if(worker.available)
         {
-            console.log(worker.Name+` `+worker.surname)
+            console.log(worker.Name+` `+worker.surname);
         }        
 
     }
 
+}
+
+function getWorkersNamesByCategory(cat:Category)
+{
+
+    let workers:Array<string> =[];
+
+    for(var worker of getAllworkers()){
+
+        if(worker.category == cat)
+        {
+            workers.push(worker.Name+` `+worker.surname);
+        }        
+
+    }
+
+    return workers;
 
 }
 
-logFirstAvailable(getAllworkers())
+function logWorkersNames(workers:string[]):void
+{
+
+    console.log(workers)
+
+}
+
+/*let arrowPrintDeveloper = (workers:any[])=> workers.forEach(if(worker.category == cat)
+{
+    workers.push(worker.Name+` `+worker.surname);
+}    )*/
+
+/*let arrowPrintDeveloper = getAllworkers().forEach(worker => {if(worker.category == Category.Developer)
+{
+    console.log(worker.Name+` `+worker.surname);
+} })*/
+
+console.log("Task 1.1");
+logFirstAvailable(getAllworkers());
+
+console.log("Task 1.2");
+logWorkersNames(getWorkersNamesByCategory(Category.Analyst));
+
+console.log("Task 1.3");
+//arrowPrintDeveloper
+
+getAllworkers().forEach(worker => {if(worker.category == Category.Developer)
+    {
+        console.log(worker.Name+` `+worker.surname);
+    } })
+
