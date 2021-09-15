40e547d3abd179e43b5dedd4b53e27e344db73c0
Nerdan <01castle02@gmail.com>
2021-09-15 04:36:46 +0300

    Мне просто нужен милый, ревнивый мальчик-омега

--- Lab1/Task1-1.js
+++ Lab1/Task1-1.js
@@ -7,36 +7,41 @@ var Category;
     Category["Scrum"] = "Scrum master";
 })(Category || (Category = {}));
 function getAllworkers() {
-    var workers = [
+    let workers = [
         { id: 1, Name: 'Ivan', surname: 'Ivanov', available: true, salary: 1000, category: Category.Analyst },
-        { id: 1, Name: 'Petro', surname: 'Petrov', available: true, salary: 1500, category: Category.Analyst },
-        { id: 1, Name: 'Vasyl', surname: 'Vasyliev', available: false, salary: 1600, category: Category.Scrum },
-        { id: 1, Name: 'Evgen', surname: 'Zhukov', available: true, salary: 1300, category: Category.Developer }
+        { id: 2, Name: 'Petro', surname: 'Petrov', available: true, salary: 1500, category: Category.Analyst },
+        { id: 3, Name: 'Vasyl', surname: 'Vasyliev', available: false, salary: 1600, category: Category.Scrum },
+        { id: 4, Name: 'Evgen', surname: 'Zhukov', available: true, salary: 1300, category: Category.Developer }
     ];
     return workers;
 }
 function logFirstAvailable(workers) {
     //let workersNum:Number = workers.length
     console.log(workers.length);
-    for (var _i = 0, workers_1 = workers; _i < workers_1.length; _i++) {
-        var worker = workers_1[_i];
+    for (var worker of workers) {
         if (worker.available) {
-            console.log(worker.Name + " " + worker.surname);
+            console.log(worker.Name + ` ` + worker.surname);
         }
     }
 }
 function getWorkersNamesByCategory(cat) {
-    var workers = [];
-    for (var _i = 0, _a = getAllworkers(); _i < _a.length; _i++) {
-        var worker = _a[_i];
+    let workers = [];
+    for (var worker of getAllworkers()) {
         if (worker.category == cat) {
-            workers.push(worker.Name + " " + worker.surname);
+            workers.push(worker.Name + ` ` + worker.surname);
         }
     }
     return workers;
 }
 function logWorkersNames(workers) {
     console.log(workers);
+}
+function getWorkerByID(id) {
+    let worker = [];
+    worker.push(getAllworkers().find(el => el.id == id).Name);
+    worker.push(getAllworkers().find(el => el.id == id).surname);
+    worker.push(getAllworkers().find(el => el.id == id).salary);
+    return worker;
 }
 /*let arrowPrintDeveloper = (workers:any[])=> workers.forEach(if(worker.category == cat)
 {
@@ -46,11 +51,26 @@ function logWorkersNames(workers) {
 {
     console.log(worker.Name+` `+worker.surname);
 } })*/
+function createCustomerID(name, id) {
+    return name + " " + id;
+}
+console.log("Task 1.1");
 logFirstAvailable(getAllworkers());
+console.log("Task 1.2");
 logWorkersNames(getWorkersNamesByCategory(Category.Analyst));
+console.log("Task 1.3");
 //arrowPrintDeveloper
-getAllworkers().forEach(function (worker) {
+getAllworkers().forEach(worker => {
     if (worker.category == Category.Developer) {
-        console.log(worker.Name + " " + worker.surname);
+        console.log(worker.Name + ` ` + worker.surname);
     }
 });
+console.log(getWorkerByID(3));
+console.log("Task 1.4");
+let myId = createCustomerID("Ann", 10);
+console.log(myId);
+//let IdGenerator:string = (name:string, id:number) => {return name+" "+id};
+let IdGenerator = (name, id) => { return name + " " + id; };
+console.log(IdGenerator("Stes", 24));
+IdGenerator = createCustomerID;
+console.log(IdGenerator("Aik", 13));
--- Lab1/Task1-1.ts
+++ Lab1/Task1-1.ts
@@ -5,14 +5,14 @@ function getAllworkers()
 {
     let workers = [
         {id: 1, Name: 'Ivan', surname: 'Ivanov', available: true, salary: 1000, category: Category.Analyst},
-        {id: 1, Name: 'Petro', surname: 'Petrov', available: true, salary: 1500, category: Category.Analyst},
-        {id: 1, Name: 'Vasyl', surname: 'Vasyliev', available: false, salary: 1600, category: Category.Scrum},
-        {id: 1, Name: 'Evgen', surname: 'Zhukov', available: true, salary: 1300, category: Category.Developer}
+        {id: 2, Name: 'Petro', surname: 'Petrov', available: true, salary: 1500, category: Category.Analyst},
+        {id: 3, Name: 'Vasyl', surname: 'Vasyliev', available: false, salary: 1600, category: Category.Scrum},
+        {id: 4, Name: 'Evgen', surname: 'Zhukov', available: true, salary: 1300, category: Category.Developer}
     ]
     return workers;
 }
    
-function logFirstAvailable(workers:any[]):void
+function logFirstAvailable(workers:any[] = getAllworkers()):void
 {
     //let workersNum:Number = workers.length
     console.log(workers.length)
@@ -28,7 +28,7 @@ function logFirstAvailable(workers:any[]):void
 
 }
 
-function getWorkersNamesByCategory(cat:Category)
+function getWorkersNamesByCategory(cat:Category = Category.Designer)
 {
 
     let workers:Array<string> =[];
@@ -53,6 +53,19 @@ function logWorkersNames(workers:string[]):void
 
 }
 
+function getWorkerByID (id:number)
+{
+
+    let worker:any[] = [];
+
+    worker.push(getAllworkers().find(el => el.id==id).Name)
+    worker.push(getAllworkers().find(el => el.id==id).surname)
+    worker.push(getAllworkers().find(el => el.id==id).salary)
+
+    return worker
+
+}
+
 /*let arrowPrintDeveloper = (workers:any[])=> workers.forEach(if(worker.category == cat)
 {
     workers.push(worker.Name+` `+worker.surname);
@@ -62,6 +75,48 @@ function logWorkersNames(workers:string[]):void
 {
     console.log(worker.Name+` `+worker.surname);
 } })*/
+
+function createCustomerID(name:string, id:number):string
+{
+
+    return name+" "+id
+
+}
+
+function createCustomer(name:string, age?:number, city?:string)
+{
+    if(age && city)
+    {
+
+        console.log(`Name: ${name}, age: ${age}, city: ${age}`)
+
+    }
+    else if(age)
+    {
+
+        console.log(`Name: ${name}, age: ${age}`)
+
+    }
+    else if(city)
+    {
+
+        console.log(`Name: ${name}, city: ${age}`)
+
+    }
+    else
+    {
+
+        console.log(`Name: ${name}`)
+
+    }
+}
+
+function сheckoutWorkers(customer:string, ...workerIDs:number[])
+{
+
+
+
+}
 
 console.log("Task 1.1");
 logFirstAvailable(getAllworkers());
@@ -77,3 +132,21 @@ getAllworkers().forEach(worker => {if(worker.category == Category.Developer)
         console.log(worker.Name+` `+worker.surname);
     } })
 
+console.log(getWorkerByID(3));
+
+
+console.log("Task 1.4");
+let myId:string = createCustomerID("Ann",10);
+console.log(myId);
+//let IdGenerator:string = (name:string, id:number) => {return name+" "+id};
+let IdGenerator = (name:string, id:number) => {return name+" "+id};
+console.log(IdGenerator("Stes",24));
+IdGenerator = createCustomerID;
+console.log(IdGenerator("Aik",13));
+
+console.log("Task 1.5");
+createCustomer("Vlad");
+createCustomer("Vlad", 9);
+createCustomer("Vlad",9,"Old fragmentation city");
+getWorkersNamesByCategory();
+logFirstAvailable();
