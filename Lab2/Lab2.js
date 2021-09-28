dca696092911255eedac1d4e81c36c56023ff67a
Nerdan <01castle02@gmail.com>
2021-09-29 02:06:19 +0300

    ищю вакансию профессионального прокрастинатора

--- Lab2/Lab2.js
+++ Lab2/Lab2.js
@@ -43,11 +43,14 @@ class Encyclopedia extends ReferenceItem {
         console.log(`${this.title} was published in ${this.year} by ${ReferenceItem.department}`);
         console.log(`Edition:${this.edition}`);
     }
+    printCitation() {
+        console.log("title - year");
+    }
 }
-let ref = new ReferenceItem("1984", 1984);
+/*let ref = new ReferenceItem("1984", 1984);
 ref.publisherSetter("solidjays");
 ref.printItem();
-console.log(ref.publisherGetter());
+console.log(ref.publisherGetter());*/
 let refBook = new Encyclopedia("Kabanchik raspravil plechi", 109, 1);
 refBook.printItem();
 function prizeReal(str) {
