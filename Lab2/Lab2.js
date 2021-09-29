25331de389fd32e19eb3531e06d2b07a18e9c600
Nerdan <01castle02@gmail.com>
2021-09-29 11:16:52 +0300

    с - структуризация

--- Lab2/Lab2.js
+++ Lab2/Lab2.js
@@ -6,6 +6,26 @@ var Category;
     Category["QA"] = "QA";
     Category["Scrum"] = "Scrum master";
 })(Category || (Category = {}));
+function prizeReal(str) {
+    console.log(str);
+}
+function getAllworkers() {
+    let workers = [
+        { id: 1, Name: 'Ivan', surname: 'Ivanov', available: true, salary: 1000, category: Category.Analyst, markPrize: prizeReal },
+        { id: 2, Name: 'Petro', surname: 'Petrov', available: true, salary: 1500, category: Category.Analyst, markPrize: prizeReal },
+        { id: 3, Name: 'Vasyl', surname: 'Vasyliev', available: false, salary: 1600, category: Category.Scrum, markPrize: prizeReal },
+        { id: 4, Name: 'Evgen', surname: 'Zhukov', available: true, salary: 1300, category: Category.Developer, markPrize: prizeReal }
+    ];
+    return workers;
+}
+function getWorkerByID(id) {
+    return getAllworkers().find(el => el.id == id);
+}
+function printWorker(worker) {
+    console.log(`${worker.Name} ${worker.surname}, ${worker.salary}`);
+}
+let logPrize = prizeReal;
+console.log(logPrize("kpop"));
 let favoriteAuthor = { name: "Nill", email: "kiggers@kk.klan", numBooksPublished: 8441 };
 /*let favoriteLibrarian:Librarian = {name:"Nate", email:"higgers@waffen.ss", department:"Totenkopf", assistCustomer:function tmp(name:string) {
     console.log("alhamdulillah "+name);
@@ -53,26 +73,3 @@ ref.printItem();
 console.log(ref.publisherGetter());*/
 let refBook = new Encyclopedia("Kabanchik raspravil plechi", 109, 1);
 refBook.printItem();
-function prizeReal(str) {
-    console.log(str);
-}
-function getAllworkers() {
-    let workers = [
-        { id: 1, Name: 'Ivan', surname: 'Ivanov', available: true, salary: 1000, category: Category.Analyst, markPrize: prizeReal },
-        { id: 2, Name: 'Petro', surname: 'Petrov', available: true, salary: 1500, category: Category.Analyst, markPrize: prizeReal },
-        { id: 3, Name: 'Vasyl', surname: 'Vasyliev', available: false, salary: 1600, category: Category.Scrum, markPrize: prizeReal },
-        { id: 4, Name: 'Evgen', surname: 'Zhukov', available: true, salary: 1300, category: Category.Developer, markPrize: prizeReal }
-    ];
-    return workers;
-}
-function getWorkerByID(id) {
-    return getAllworkers().find(el => el.id == id);
-}
-function printWorker(worker) {
-    console.log(`${worker.Name} ${worker.surname}, ${worker.salary}`);
-}
-let logPrize = prizeReal;
-console.log(printWorker(getAllworkers()[1]));
-console.log(getAllworkers()[1].markPrize("123"));
-console.log(getWorkerByID(3).Name);
-console.log(logPrize("kpop"));
--- Lab2/Lab2.ts
+++ Lab2/Lab2.ts
@@ -6,6 +6,10 @@ interface PrizeLogger
     (str:string):void; 
 }
 
+function prizeReal(str:string) {
+    console.log(str)
+}
+
 interface Workers{
     id:number;
     Name:string;
@@ -15,6 +19,36 @@ interface Workers{
     category:Category;
     markPrize:PrizeLogger;
 }
+
+function getAllworkers() 
+{
+    let workers:Workers[] = [
+        {id: 1, Name: 'Ivan', surname: 'Ivanov', available: true, salary: 1000, category: Category.Analyst, markPrize:prizeReal},
+        {id: 2, Name: 'Petro', surname: 'Petrov', available: true, salary: 1500, category: Category.Analyst, markPrize:prizeReal},
+        {id: 3, Name: 'Vasyl', surname: 'Vasyliev', available: false, salary: 1600, category: Category.Scrum, markPrize:prizeReal},
+        {id: 4, Name: 'Evgen', surname: 'Zhukov', available: true, salary: 1300, category: Category.Developer, markPrize:prizeReal}
+    ]
+    return workers;
+}
+ 
+
+function getWorkerByID (id:number)
+{
+
+    return getAllworkers().find(el => el.id==id)
+
+}
+
+function printWorker(worker:Workers)
+{
+
+    console.log(`${worker.Name} ${worker.surname}, ${worker.salary}`)
+
+}
+
+let logPrize:PrizeLogger= prizeReal;
+console.log(logPrize("kpop"));
+
 
 interface Person
 {
@@ -111,7 +145,6 @@ class Encyclopedia extends ReferenceItem{
     constructor(title:string, year:number, public edition:number)
     {
         super(title, year);
-
     }
 
     printItem() {
@@ -134,41 +167,4 @@ console.log(ref.publisherGetter());*/
 let refBook = new Encyclopedia("Kabanchik raspravil plechi", 109, 1);
 refBook.printItem();
 
-function prizeReal(str:string) {
-    console.log(str)
-}
 
-function getAllworkers() 
-{
-    let workers:Workers[] = [
-        {id: 1, Name: 'Ivan', surname: 'Ivanov', available: true, salary: 1000, category: Category.Analyst, markPrize:prizeReal},
-        {id: 2, Name: 'Petro', surname: 'Petrov', available: true, salary: 1500, category: Category.Analyst, markPrize:prizeReal},
-        {id: 3, Name: 'Vasyl', surname: 'Vasyliev', available: false, salary: 1600, category: Category.Scrum, markPrize:prizeReal},
-        {id: 4, Name: 'Evgen', surname: 'Zhukov', available: true, salary: 1300, category: Category.Developer, markPrize:prizeReal}
-    ]
-    return workers;
-}
- 
-
-function getWorkerByID (id:number)
-{
-
-    return getAllworkers().find(el => el.id==id)
-
-}
-
-function printWorker(worker:Workers)
-{
-
-    console.log(`${worker.Name} ${worker.surname}, ${worker.salary}`)
-
-}
-
-
-
-let logPrize:PrizeLogger= prizeReal;
-
-console.log(printWorker(getAllworkers()[1]));
-console.log(getAllworkers()[1].markPrize("123"));
-console.log(getWorkerByID(3).Name);
-console.log(logPrize("kpop"));
